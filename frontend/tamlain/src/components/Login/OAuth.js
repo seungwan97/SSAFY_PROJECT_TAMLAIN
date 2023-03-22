f3fe489dafe3ea7d83e7630576f8071e66a1b5d0
김대은 <eodms090@naver.com>
2023-03-22 15:39:33 +0900

    fix: uri 수정

--- frontend/tamlain/src/components/Login/OAuth.js
+++ frontend/tamlain/src/components/Login/OAuth.js
@@ -1,5 +1,6 @@
 const REST_API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
 // const REDIRECT_URI = "https://localhost:3000/oauth/callback/kakao"; // 프론트에서 접근가능한 URI여야한다.
-const REDIRECT_URI = process.env.REACT_APP_SERVER_URI;
+
+const REDIRECT_URI = process.env.REACT_APP_SERVER_URI+"/oauth/callback/kakao";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
