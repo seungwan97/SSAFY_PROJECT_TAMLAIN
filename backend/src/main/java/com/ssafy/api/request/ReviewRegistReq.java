49a57dc424dd3c52a73b2c06dccb757f14665471
장별하 <daisy1080@naver.com>
2023-03-29 02:23:42 +0900

    swagger 설정

--- backend/src/main/java/com/ssafy/api/request/ReviewRegistReq.java
+++ backend/src/main/java/com/ssafy/api/request/ReviewRegistReq.java
@@ -1,11 +1,16 @@
 package com.ssafy.api.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 import java.util.List;
 
+@ApiModel(description = "리뷰(별점) 등록 정보가 포함된 Request")
 @Getter
 public class ReviewRegistReq {
+    @ApiModelProperty(value = "사용자 id")
     private int userId;
+    @ApiModelProperty(value = "모든 장소에 대한 리뷰(별점) 목록")
     private List<ReviewRegistItem> reviewRegistItemList;
 }
--- backend/src/main/java/com/ssafy/api/request/ScheduleRegistReq.java
+++ backend/src/main/java/com/ssafy/api/request/ScheduleRegistReq.java
@@ -1,14 +1,22 @@
 package com.ssafy.api.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 import java.util.List;
 
+@ApiModel(description = "등록할 일정 정보가 포함된 Request")
 @Getter
 public class ScheduleRegistReq {
+    @ApiModelProperty(value = "사용자 id")
     private int userId;
+    @ApiModelProperty(value = "설문조사 id")
     private int surveyId;
+    @ApiModelProperty(value = "일정 썸네일 id")
     private int scheduleThumbnailId;
+    @ApiModelProperty(value = "일정명")
     private String name;
+    @ApiModelProperty(value = "사용자가 선택한 장소 목록")
     private List<ScheduleRegistItem> scheduleRegistItemList;
 }
