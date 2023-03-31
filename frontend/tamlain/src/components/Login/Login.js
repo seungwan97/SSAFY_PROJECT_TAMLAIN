993bdc2e2c0592eb9732ab4971754ac542dcdb06
xxknu88 <xxknu88@naver.com>
2023-03-31 14:27:02 +0900

    fix : 수리

--- frontend/tamlain/src/components/Login/Login.js
+++ frontend/tamlain/src/components/Login/Login.js
@@ -1,19 +1,26 @@
 import React from "react";
 import * as S from "./Login.styled";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { KAKAO_AUTH_URL } from "./OAuth";
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const kakaoLogin = () => {
     window.location.href = KAKAO_AUTH_URL;
+  };
+
+  const redirectMainPage = () => {
+    navigate("/");
   };
 
   return (
     <S.Container>
       <S.Filter />
-      {/* <S.goBack>
-        <Link to="/">메인페이지</Link>
-      </S.goBack> */}
+      <S.Logo
+        src={`${process.env.PUBLIC_URL}/assets/MainLogo.png`}
+        onClick={redirectMainPage}
+      />
       <S.KakaoBtnContainer onClick={kakaoLogin}>
         <S.KakaoImg
           src={`${process.env.PUBLIC_URL}/assets/Icon/kakao_login_medium_wide.png`}
--- frontend/tamlain/src/utils/client.js
+++ frontend/tamlain/src/utils/client.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const client = axios.create();
 
 // 로컬 테스트 용
-// client.defaults.baseURL = "http://localhost:8080";
+client.defaults.baseURL = "http://localhost:8080";
 
 // 배포 서버 용
-client.defaults.baseURL = "https://j8b204.p.ssafy.io/api";
+// client.defaults.baseURL = "https://j8b204.p.ssafy.io/api";
 
 export default client;
