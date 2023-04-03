3bb94bd634a1f4c031cbbedb43aee0717a484afe
yubeom Jang <xxknu88@naver.com>
2023-04-04 07:28:35 +0900

    feat : 마이페이지

--- frontend/tamlain/src/components/MyPage/MyPageDetail.js
+++ frontend/tamlain/src/components/MyPage/MyPageDetail.js
@@ -20,29 +20,52 @@ const MyPageDetail = () => {
     period: "",
   });
 
-  console.log(scheduleId);
+  // console.log(scheduleId);
 
+
+  // axios로 데이터 전부 가져오기 
   useEffect(() => {
     let mypageInfo = {};
     getScheduleDetail(key, scheduleId).then((res) => {
       console.log(res);
+
+      const data = res.data.data.mypageCommonInfo;
       mypageInfo = {
-        title: res.data.data.mypageCommonInfo.name,
-        startDate: res.data.data.mypageCommonInfo.startDate.replaceAll(
+        title: data.name,
+        startDate: data.startDate.replaceAll(
           "-",
           "."
         ),
-        endDate: res.data.data.mypageCommonInfo.endDate.replaceAll("-", "."),
-        period:
-          res.data.data.mypageCommonInfo.period -
-          1 +
-          "박 " +
-          res.data.data.mypageCommonInfo.period +
-          "일",
+        endDate: data.endDate.replaceAll("-", "."),
+        period: data.period -1 +  "박 " + data.period + "일",
       };
       setMypageCommonInfo(mypageInfo);
+      
+      // ----- 지도 부분 데이터 가공 ------
+
+      // period로 일차 가져오기
+      console.log(res.data.data.mypageCommonInfo.period);
+      let day = res.data.data.mypageCommonInfo.period; 
+
+      //scheduleDetailItemMap[period]  -> 1~N일차 돌리기
+      console.log(res.data.data.scheduleDetailItemMap[1].length);
+
+      // for (let i = 0; i < day; i++){
+      //   let placeCount = res.data.data.scheduleDetailItemMap[i].length;
+      //   for (let j = 0; j < placeCount; j++){
+          
+      //   }  
+      // }
+      
+      //res.data.data.scheduleDetailItemMap[1].length -> 일차별 장소개수
+      //res.data.data.scheduleDetailItemMap[1][0~개수] -> 장소개수만큼 돌리기 
+      //res.data.data.scheduleDetailItemMap[][].mapInfo.title -> 장소명
+
+      // console.log(res.data.data.scheduleDetailItemMap[1][0].mapInfo.title);
+
     });
   }, []);
+
 
   // 뒤로 가기 버튼
   const redirectPage = () => {
@@ -51,6 +74,20 @@ const MyPageDetail = () => {
 
   // 타이틀 수정 버튼
   const updateTitle = () => {};
+
+  
+//  -------카카오 지도 -----------
+  const { kakao } = window;
+
+  useEffect(() => {
+    const container = document.getElementById('map'); 
+    const options = {
+      center: new kakao.maps.LatLng(33.510575, 126.491139),
+      level: 6
+    };
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+
 
   return (
     <>
@@ -77,6 +114,42 @@ const MyPageDetail = () => {
           </S.TextTwo>
           <S.TextThree>{mypageCommonInfo.period}</S.TextThree>
         </S.FlexBox>
+        {/* ----------- 일정명 , 여행 날짜 -----------  */}
+
+        {/* 지도 시작  */}
+          <div> N일차 </div>
+        <S.MapContainer>
+
+          <div id="map"  style={{
+          float: "left",
+          width: "80%",
+          height: "350px",
+          marginTop: "5%",
+        }}></div>
+          
+          <S.Div>
+          <div
+            style={{
+              width: "3px",
+              height: "100vh",
+              marginLeft: "15%",
+              backgroundColor: "#fc872a",
+            }}
+          ></div>
+          <div
+            id="tagArea"
+            style={{
+              position: "absolute",
+              zIndex: "10",
+              top: "5%",
+              right: "5%",
+              marginBottom: "5px",
+            }}
+          ></div>
+          </S.Div>
+          
+        </S.MapContainer>
+
       </S.Container>
     </>
   );
